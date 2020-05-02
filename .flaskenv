bc4c6502990e99b874d031c3c84a544a6b3e9897
saikrishna-prathapaneni <57140923+saikrishna-prathapaneni@users.noreply.github.com>
2020-05-02 20:31:14 +0530

    commit

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=application.py
+FLASK_APP=new_route.py
+FlASK_ENV=development
---
+++ new_route.py
@@ -0,0 +1,21 @@
+import datetime
+from flask import Flask
+from flask import render_template
+
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+    time_date=datetime.datetime.now()
+    time=time_date.time()
+    headline ="hello world! now the time is "
+    return render_template("styles_css.html",headline=headline,time=time)
+
+@app.route('/more')
+def today_time():
+    today=datetime.datetime.now()
+    today_date=today.isocalendar()
+    return render_template("more.html",today_date=today_date)
+    
